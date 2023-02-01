f36365b7c8b60dcf441bb51007561b2f19ba50ef
domieee <99103235+domieee@users.noreply.github.com>
2023-02-01 13:39:03 +0100

    init

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+let checkNumber = [
+    18,
+    16,
+    80,
+    51,
+    47,
+    38,
+    95,
+    42,
+    68,
+    61,
+    34,
+    51,
+    20,
+    17,
+    56,
+    31,
+    100,
+    6,
+    5,
+    30,
+    74,
+    97,
+    28,
+    99,
+    91,
+    27,
+    73,
+    12,
+    92,
+    6,
+    27,
+    71,
+    26,
+    15,
+    78
+];
+
+// TODO: Verwende die Variable checkNumber und map(). Wenn sich die Zahl durch 3 teilen lässt, dann soll zu ihr 100 addiert werden.
+
+//      - Verwende den Code aus dem Kommentarbereich.
+//      - Verwende ein if Statement, um zu überprüfen, ob die Zahl durch 3 teilbar ist.
+//      - Wenn ja, addiere 100 zu dieser Zahl hinzu.
+//      - Gib das Resultat in der Konsole aus.
+
+const checkEach = checkNumber.map(elem => {
+    if (elem % 3 === 0) {
+        return `${elem}` + ' ' + 'can be divided by 3'+ ' - ' + `${elem}` +  ' + 100 = ' + (elem + 100); 
+    }   else {
+        return `${elem}` + ' ' + 'cannot be divided by 3';
+    }
+})
+
+console.log(checkEach);
